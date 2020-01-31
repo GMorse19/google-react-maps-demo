7bd4acf22a8473bbb54c90e7cef949317aaa679a
GMorse19 <malorse@comcast.net>
2020-01-30 22:58:25 -0500

    Worked on center of map when map moves

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react'
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
 import TestComponent from './TestComponent'
+// import { fitBounds } from 'google-map-react/utils';
 
 class App extends React.Component {
 constructor() {
   super()
   this.state = {
-    center: '',
+    center: null,
     coordinates: {
       lat: null,
       lng: null
@@ -19,8 +20,6 @@ constructor() {
     showWindow: false,
     userLocation: { lat: 42.3601, lng: -71.0589}
   }
-  console.log('Center Point: ' + this.state.center)
-  console.log('User Data: ' + this.state.query)
 }
 
 // set query to find Place data
@@ -51,11 +50,22 @@ onInfoWindowClose = () => {
   this.setState({ showWindow: false })
 }
 
-setCenter = async center => {
-  const results = await geocodeByAddress(center)
-  const coordinates = await getLatLng(results[0])
-  this.setState({ center: coordinates })
+clickMap = (mapProps, map, clickEvent) => {
+  console.log('clicked map')
+  console.log(clickEvent)
+  console.log(clickEvent.placeId)
+  console.log('Center Point: ' + clickEvent.center)
+}
+
+centerMoved = (mapProps, map) => {
+  this.setState({ center: map.getCenter() })
+  console.log('map.getCenter() is: ' + map.getCenter())
   console.log('Center Point: ' + this.state.center)
+}
+
+onMouseoverMarker(props, marker, e) {
+  console.log(marker)
+  console.log(props)
 }
 
 render() {
@@ -102,15 +112,21 @@ render() {
         </PlacesAutocomplete>
 
         <Map google={this.props.google}
-             center={this.state.coordinates}
+             center={this.state.center}
              initialCenter={this.state.userLocation}
              zoom={14}
+             clickableIcons={true}
+             onClick={this.clickMap}
+             onDragend={this.centerMoved}
         >
 
           <Marker onClick={this.onMarkerClick}
                   position={this.state.coordinates}
                   name={'Current location'}
           />
+
+          <Marker onMouseover={this.onMouseoverMarker}
+          name={'Current location'} />
 
           <InfoWindow marker={this.state.selectedMarker}
                       position={this.state.coordinates}
